1c7a55f4a41ad117eb53373170b3b76b3a296768
Ehsan Rasul <ehsanrasul0@gmail.com>
2023-05-13 08:36:11 +0500

    Login Signup Implemented Without JWT

---
+++ app-backend/routes/AuthorRoutes/author.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Author = require('../../models/AuthorModel/authorSchema')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+
+
+//Signup Route
+app.post("/signup", async (req, res) => {
+  try {
+    const author = await new Author(req.body);
+    author
+      .save()
+      .then((response) => {
+        console.log(response);
+        res.status(200).json({ status: response });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (error) {
+    res.json({ status: "error" });
+  }
+});
+
+
+// Login Route
+app.post("/login", async (req, res) => {
+  const author = await Author.findOne({
+    username: req.body.username,
+    password: req.body.password
+  })
+    .then((obj) => {
+      if (obj) {
+        res.json({ found: true, object: obj });
+      } else {
+        res.json({ found: false });
+      }
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+
+module.exports = app;
--- app-backend/server.js
+++ app-backend/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
+const port = 3000;
+var mongoose = require("mongoose");
+const cors = require("cors");
+app.use(cors());
+const dotenv = require("dotenv");
+app.use(express.json());
+
+
 require("dotenv").config();
+
+const authorRouter = require('./routes/AuthorRoutes/author.js')
+//const inventoryRouter = require('./Routes/inventoryRoutes')
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
+
+
+//Routes
+app.use("/api/author", authorRouter)
+//app.use("/api/inventory", inventoryRouter)
+
 
 mongoose
   .connect(MONGODB_URI, {
